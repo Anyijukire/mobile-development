b19578d91f1441403a21be0627759d005932fd54
Anyijukire <anyijukirjanett@gmail.com>
2021-02-21 10:37:17 +0300

    first commit

---
+++ src/student records.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var studentName= ("janetSandra")
+    print(studentName)
+    var age= 16
+    print(age)
+    var phone= ("0778844665")
+    print(phone)
+    var weight= 60.57686879
+    print(weight)
+    var citizen= ("false")
+    print(citizen)
+    var name= arrayOf("janetSandra",16,"0778844665",60.576879,"false")
+    print(name)
+}
